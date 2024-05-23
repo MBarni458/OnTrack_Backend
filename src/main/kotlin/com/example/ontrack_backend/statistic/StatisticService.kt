e1960930b8668e5abfb9a883e4b7ca9f065d3846
MBarni458 <mbarni458@gmail.com>
2024-05-24 01:28:56 +0200

    cleaner statistics

--- src/main/kotlin/com/example/ontrack_backend/statistic/StatisticService.kt
+++ src/main/kotlin/com/example/ontrack_backend/statistic/StatisticService.kt
@@ -1,6 +1,5 @@
 package com.example.ontrack_backend.statistic
 
-import com.example.ontrack_backend.repository.ActivityRepository
 import com.example.ontrack_backend.repository.SubActivityRepository
 import com.example.ontrack_backend.repository.UserRepository
 import com.example.ontrack_backend.statistic.dataclasses.TypeAndCount
@@ -9,27 +8,25 @@ import org.springframework.stereotype.Service
 import kotlin.math.min
 
 @Service
-class StatisticService @Autowired public constructor(val userRepository: UserRepository, val activityRepository: ActivityRepository, val subActivityRepository: SubActivityRepository) {
+class StatisticService @Autowired public constructor(
+    val userRepository: UserRepository,
+    val subActivityRepository: SubActivityRepository
+) {
 
-    fun getTopTen(): Map<String,Int>{
-        val users= userRepository.findAll().toList().sortedByDescending { it.getPoints() }
-        return users.subList(0, min(users.size, 9)).associate { it.username to it.getPoints() }
+    fun getTopTen(): Map<String, Int> = userRepository.findAll().toList().sortedByDescending { it.getPoints() }.run {
+        subList(0, min(size, 9)).associate { it.username to it.getPoints() }
     }
 
-    fun getPopularType(type:String): TypeAndCount {
-        return try{
-            val foods = subActivityRepository.findAll().filter { it.type==type }
-            val popularity = HashMap<String, Int>();
-            for (food in foods){
-                popularity[food.name] = popularity[food.name]?.plus(1) ?: 1
-            }
-        val max=popularity.maxBy { it.value }.toPair()
-            TypeAndCount(max.first,max.second)
-        } catch (e:Exception){
-            TypeAndCount(type,0)
+    fun getPopularType(type: String): TypeAndCount {
+        val max = subActivityRepository.findAll().filter { it.type == type }.groupingBy { it.name }.eachCount()
+            .maxByOrNull { it.value }?.toPair()
+        return if (max != null) {
+            TypeAndCount(max.first, max.second)
+        } else {
+            TypeAndCount(type, 0)
         }
     }
 
-    fun getAverageScore()= userRepository.findAll().map { it.getPoints() }.average()
+    fun getAverageScore() = userRepository.findAll().map { it.getPoints() }.average()
 
 }
